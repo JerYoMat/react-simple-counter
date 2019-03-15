0f96a03b3794ecb627a5bc6d6276974490f74bf0
JerYOMat <jeremy.emata@gmail.com>
2019-03-15 02:34:24 -0400

    added basic counter that tracks the number times the button has been clicked in parent component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Child({ onAction }) {
+  return (
+    <button onClick={onAction}>
+      Click to increment
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class CountingParent extends React.Component {
+  state = {
+    actionCount: 0
+  }
+  handleAction = (action) => {
+    this.setState({
+      actionCount: this.state.actionCount + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child onAction={this.handleAction} />
+        <p>Clicked {this.state.actionCount} times</p>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+ReactDOM.render(<CountingParent />, document.getElementById('root'));
+
+
